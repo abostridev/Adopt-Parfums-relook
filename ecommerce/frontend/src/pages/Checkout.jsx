6e82866078e3c3c43b8dc8fb27384d4600ac9128
astridev <abostridev@gmail.com>
2026-01-16 13:10:16 +0100

    style number input

--- ecommerce/frontend/src/pages/Checkout.jsx
+++ ecommerce/frontend/src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import PhoneInput from "react-phone-number-input";
-import "react-phone-number-input/index.css";
+import "react-phone-number-input/style.css";
 import { isValidPhoneNumber } from "react-phone-number-input";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
